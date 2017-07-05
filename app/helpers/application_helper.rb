327a28f83d17a91ac0aca767869a8192a39fe9a2
Jordan Hudgens <jordan@devcamp.com>
2017-07-05 17:07:25 +0000

    Gemfile edits.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+  
+  def copyright_generator
+    LegendViewTool::Renderer.copyright 'Aakash Kulkarni', 'All Rights Reserved'
+  end
 end
