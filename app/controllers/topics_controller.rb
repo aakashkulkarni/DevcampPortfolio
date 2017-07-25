bd5b214f92aa664399c0951bcf770300bc8e34d8
Aakash Kulkarni <kashakoolsmart@gmail.com>
2017-07-25 02:43:11 +0000

    Implemented CRUD functionality for topics on blogs page

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,9 @@
 class TopicsController < ApplicationController
+  before_action :set_topic
+  skip_before_action :set_topic, only: [:index , :new, :create]
   before_action :set_sidebar_topics
   layout 'blog'
+  
   def index
     @topics = Topic.all
   end
@@ -14,9 +17,60 @@ class TopicsController < ApplicationController
     end
   end
   
+  def new
+    @topic = Topic.new
+  end
+  
+  def edit
+  end
+  
+  def create
+    @topic = Topic.new(topic_params)
+
+    respond_to do |format|
+      if @topic.save
+        format.html { redirect_to @topic, notice: 'New topic created!' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+  
+  def update
+    @topic = Topic.find(params[:id])
+    
+    respond_to do |format|
+      if @topic.update(topic_params)
+        format.html { redirect_to @topic, notice: 'Topic was successfully updated.' }
+        format.json { render :show, status: :ok, location: @topic }
+      else
+        format.html { render :edit }
+        format.json { render json: @topic.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+  def destroy
+    @topic.destroy
+    
+    respond_to do |format|
+      format.html { redirect_to topics_url, notice: 'Topic was deleted.' }
+      format.json { head :no_content }
+    end
+  end
+  
   private
   
-  def set_sidebar_topics
-    @side_bar_topics = Topic.with_blogs 
-  end
+    def set_sidebar_topics
+      @side_bar_topics = Topic.with_blogs 
+    end
+    
+    def topic_params
+      params.require(:topic).permit(:title)
+    end
+    
+    def set_topic
+      @topic = Topic.find(params[:id])
+    end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :topics, only: [:index, :show]
+  resources :topics, only: [:index, :show, :edit, :update, :create, :destroy, :new]
+  resources :portfolio, only: [:destroy]
 
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolios, except: [:show] do
